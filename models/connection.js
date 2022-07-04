a98cea40fcbc69f8e28c526dfd62520219bd1a85
Andre Enes <andre.ferreira.enes@gmail.com>
2022-07-04 16:07:15 +0100

    sslOn

--- models/connection.js
+++ models/connection.js
@@ -5,11 +5,11 @@ const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
     max: 10
-    //,
-    //ssl: {
-    //    require: true, 
-    //    rejectUnauthorized: false
-    //}
+    ,
+    ssl: {
+        require: true, 
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = pool;
